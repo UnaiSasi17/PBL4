7101229b658466f696f4602427f2c71a65a50022
gorka etxebarria <gorka.etxebarria@gmail.com>
2022-05-27 15:11:57 +0200

    regresionJupyterNotebook

--- Estadistica/RegresionEntrenada.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-
-x=pd.read_csv("/Users/gorkaetxebarriaatutxa/Documents/PBL4/Estadistica/Datos.csv",delimiter=';')
-coefi=x['wasted'].corr(x['incomes'],method='pearson')
-print(coefi)
-
